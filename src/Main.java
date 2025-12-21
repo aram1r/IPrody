6456758cdd6c3fa5d3a7b40531c85cf44e744724
Alex Rogov <ar4m1r63@gmail.com>
2025-12-21 19:46:36 +0400

    Lesson 19 Работа с потоками и файлами

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,18 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        FileLoggerConfiguration config = new FileLoggerConfiguration(Level.DEBUG, new Pattern(), Paths.get("D:\\Programming\\IPrody\\"), "log", 15048L);
+//        FileLoggerConfiguration config = new FileLoggerConfiguration(Level.DEBUG, new Pattern(), Paths.get("D:\\Programming\\IPrody\\"), "log", 15048L);
 
-        FileLogger log = new FileLogger(config);
+//        FileLogger log = new FileLogger(config);
+//        for (int i = 0; i<1000; i++) {
+//            log.debug("Message");
+//        }
+        FileLoggerConfiguration config2= new FileLoggerConfigurationLoaderImpl().load("D:\\Programming\\IPrody\\IPrody\\config.txt");
+
+        FileLogger log = new FileLogger(config2);
         for (int i = 0; i<1000; i++) {
             log.debug("Message");
         }
-        FileLoggerConfiguration config2= FileLoggerConfigurationLoaderInterface.load("D:\\Programming\\IPrody\\IPrody\\config.txt");
         System.out.println("Программа выполнена");
     }
 }
--- src/logger/FileLoggerConfigurationLoaderInterface.java
+++ src/logger/FileLoggerConfigurationLoaderInterface.java
@@ -1,31 +1,7 @@
 package logger;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public interface FileLoggerConfigurationLoaderInterface{
 
-    static FileLoggerConfiguration load(String stringPath) {
-        FileLoggerConfiguration config = new FileLoggerConfiguration();
-        try {
-            Path path = Paths.get(stringPath);
-            Files.lines(path).forEach(e -> {
-                String setting = e.substring(0, e.indexOf("="));
-
-                //Level level, Pattern pattern, Path path, String fileName, Long maximumFileSize
-                switch (setting) {
-                    case "maximumFileSize": config.setMaximumFileSize(Long.parseLong(e.substring(e.indexOf("=") + 1))); break;
-                    //TODO переписать парсинг enum
-                    case "level": config.setLevel(Level.valueOf(e.substring(e.indexOf("=") + 1))); break;
-                    case "path": config.setPath(Paths.get(e.substring(e.indexOf("=") + 1))); break;
-                    case "fileName": config.setFileName(e.substring(e.indexOf("=") + 1)); break;
-                }
-                config.setPattern(new Pattern());
-            });
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return config;
-    };
+    FileLoggerConfiguration load(String stringPath);
 }
